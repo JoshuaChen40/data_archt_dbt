95eccd6a6ee087849a4577f1f1a9a550d1cd6590
JoshuaChen40 <rfed5440@gmail.com>
2024-06-13 18:11:54 +0800

    init

---
+++ image/sample_project/models/example/my_first_dbt_model.sql
@@ -0,0 +1,37 @@
+
+/*
+    Welcome to your first dbt model!
+    Did you know that you can also configure models directly within SQL files?
+    This will override configurations stated in dbt_project.yml
+
+    Try changing "table" to "view" below
+*/
+
+{{ config(materialized='table') }}
+
+with source_data as (
+
+    select null as id, 'system' AS name
+    union all
+    select 1 as id, 'Josh' AS name
+    union all
+    select 2 as id, 'Andy' AS name
+    union all
+    select 3 as id, 'Wiwi' AS name
+    union all
+    select 4 as id, 'James' AS name
+    union all
+    select 4 as id, 'Jack' AS name
+    union all
+    select 4 as id, 'Jack' AS name
+
+)
+
+select *
+from source_data
+
+/*
+    Uncomment the line below to remove records with null `id` values
+*/
+
+-- where id is not null
